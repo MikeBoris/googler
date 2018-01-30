19872aa98ee3afd032d9c87532bced3c308e455d
MikeBoris <mikeboris123@gmail.com>
2018-01-29 22:44:01 -0500

    fleshed out search-to-metric

---
+++ README.md
@@ -0,0 +1,11 @@
+search_alot
+===========
+
+## Install required packages
+```
+pip install -r requirements.txt
+```
+## run
+```python
+python search_for.py 'my_query'
+```
--- googler.py
+++ googler.py
@@ -68,3 +68,116 @@ Looking for a cat sitter? Book a 5-star cat sitter to feed and play with your ca
 
 
 	
+original query (broad term)
+
+get list of all synonymns (narrower terms)
+	lemmatize
+	lookup synonymns
+		wordnet
+	-> enhanced_query
+
+submit enhanced_query to:
+	twitter
+	google
+	Yelp
+
+retrieve results
+	min results for twitter
+	min results for google
+	min results for Yelp
+
+input query
+	e.g. cat-sitting
+
+query_class
+query processing
+takes query
+
+executes enhanced_query
+	instance dependent
+	eg
+	import requests
+	import beautifulsoup4 as BeautifulSoup
+	from textblob import TextBlob
+	#
+	# google search
+	# ----------------------
+	# query formatting
+	query = 'cat sitting'
+	split_q = query.split(' ')
+	formatted_query = '+'.join(split_q) # formatted_query = cat+sitting
+	search_string = 'https://www.google.com/search?q={}'
+	formatted_search_string = search_string.format(formatted_query)
+	# execute search
+	def execute_request(formatted_search_string):
+		results_thing = requests.get(formatted_search_string)
+		html_dump = results_thing.text
+		return html_dump
+
+	html_dump = execute_request(formatted_search_string)
+	
+	# parsed_text <- html_parser(html_dump)
+	def html_parser(html_dump, parser='html.parser'):
+		soup = BeautifulSoup(html_dump, parser)
+		parsed_text = soup.get_text()
+		return parsed_text
+
+	parsed_text = html_parser(html_dump)
+	# tokenizes
+	
+	blob = TextBlob(parsed_text)
+	# returns word_freq_dist_Object
+#**** word_freq_dist = blob.word_counts
+	#--- metrics -----------
+	# TODO: Need ideas for more metrics - ask John
+	# 
+	# extract topics
+#**** extract topics
+	# extract noun_phrases
+	print('Noun phrases: {}'.format(', '.join(blob.noun_phrases)))
+	# extract polarity
+	print('Polarity: {}'.format(blob.sentiment.polarity))
+	# extract named entities
+#**** extract named entities
+	
+	
+
+returns results_object
+
+TODO:
+generates enhanced_query
+
+parse/preprocess results
+preprocessing_class
+takes results_object
+parses
+tokenizes
+returns word_freq_dist_Object
+
+compute metrics
+metrics_class
+	instance dependent
+generate list of topics
+generate list of noun-phrases
+generate list of named entities
+
+	twitter:
+		twitter_metrics (instance of metrics_class)
+			top 10 keywords/topics <- results for twitter
+			top 10 noun-phrases <- results for twitter
+			top 10 named entities <- results for twitter
+	google:
+		google_metrics (instance of metrics_class)
+			top 10 keywords/topics <- results for google
+			top 10 noun-phrases <- results for google
+			top 10 named entities <- results for google
+	Yelp:
+		Yelp_metrics (instance of metrics_class)
+			top 10 keywords/topics <- results for Yelp
+			top 10 noun-phrases <- results for Yelp
+			top 10 named entities <- results for Yelp
+
+
+
+John, Can you help me w/ metrics to gather? I'm a bit clueless on what 
+we should be collecting -- 
