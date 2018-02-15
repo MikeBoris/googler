a400a08073bb6b2602d7ec46baba34668c68759f
MikeBoris <mikeboris123@gmail.com>
2018-02-14 22:48:25 -0500

    added scafolding + intro

---
+++ text2data.py
@@ -0,0 +1,71 @@
+# text2data
+
+'''
+TODO
+
+turn into module
+ie:
+text2data/
+	__init__.py
+	get.py
+		get1, get2, ...
+	clean.py
+	metrics.py
+
+'''
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+# function to get html
+url = 'http://www.indeed.com/jobs?q=data+scientist&l=MA'
+html = requests.get(url)
+
+# function to parse html, extract text
+	# input html as str
+	# tree = fromstring(html)
+	#	   = tree.elements
+	# NEED TO IDENTIFY RELEVANT ELEMENTS (CLASSES/IDS)
+	# 
+	# search engine:		indeed:
+	# Title Link			job title
+	# snippet				company name
+	#						job descr
+	#						company rating, etc
+	#
+	# (supposing we've identified our elements)
+	# blobs = CssSelector(tree.elements)
+	# concat blobs
+	# output blobs as str
+	# 
+
+soup = BeautifulSoup(html.text, 'html.parser')
+
+def extract_job(soup):
+	jobs = []
+	for div in soup.find_all(name='div', attrs={'class':'row'}):
+		for a in div.find_all(name='a', attrs={'data-tn-element':'jobTitle'}):
+			jobs.append(a['title'])
+	return jobs
+
+def extract_sum(soup):
+	summaries = []
+	spans = soup.findAll('span', attrs={'class':'summary'})
+	for span in spans:
+		summaries.append(span.text.strip())
+	return summaries
+
+jobs = extract_job(soup)
+summaries = extract_sum(soup)
+
+for i, j in zip(jobs, summaries):
+	print(i, j)
+
+
+# function to normalize text
+
+# function to structure text
+
+# function to build metrics
+
+
